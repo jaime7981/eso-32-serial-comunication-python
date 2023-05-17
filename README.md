61a8f2655a47dbc4055b93d707a32744d724297a
jaime7981 <jmfernandez1@miaundes.cl>
2023-05-17 18:19:32 -0400

    keyboard interrupt handler for threads

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # esp-32-serial-comunication-python
+
+## Python Serial Comunication Interface
+
+Basic command usage:
+`python ./py_serial/pyserial_test.py [serial_port] [serial_baudrate]`
--- py_serial/pyserial_test.py
+++ py_serial/pyserial_test.py
@@ -27,37 +27,55 @@ class SerialCommunication():
 
 def parse_args():
     port, baudrate = None, None
-    if len(sys.argv) == 2:
-        port, baudrate = sys.argv[0], sys.argv[1]
-    elif len(sys.argv) == 1:
-        port = sys.argv[0]
+    if len(sys.argv) == 3:
+        port, baudrate = sys.argv[1], sys.argv[2]
+    elif len(sys.argv) == 2:
+        port = sys.argv[1]
     return (port, baudrate)
 
-def serial_constant_reading(serial_con):
-        while True:
+def serial_constant_reading(serial_con, run_event):
+        while run_event.is_set():
             serial_data = serial_con.serial_readline()
             if serial_data != '':
                 print("Reviced ->", serial_data)
 
-def serial_constant_writing(serial_con):
-    while True:
+def serial_constant_writing(serial_con, run_event):
+    while run_event.is_set():
         serial_con.serial_write('Sent -> Serial test communication')
         time.sleep(2)
 
-def main():
-    port, baudrate = parse_args()
+def serial_cli_input_writing(serial_con, run_event):
+    while run_event.is_set():
+        try:
+            user_input = input()
+        except:
+            user_input = 'Err:Intr'
+        serial_con.serial_write(user_input)
 
-    serial_communication = SerialCommunication(port, baudrate)
-
-    reading_thread = threading.Thread(target=serial_constant_reading, args=(serial_communication,))
-    writing_thread = threading.Thread(target=serial_constant_writing, args=(serial_communication,))
-
+def create_communication_threads(serial_communication):
+    run_event = threading.Event()
+    run_event.set()
+    reading_thread = threading.Thread(target=serial_constant_reading, args=(serial_communication, run_event))
+    writing_thread = threading.Thread(target=serial_cli_input_writing, args=(serial_communication, run_event))
     reading_thread.start()
     writing_thread.start()
+    return (reading_thread, writing_thread, run_event)
 
-    reading_thread.join()
-    writing_thread.join()
-    exit(0)
+def main():
+    port, baudrate = parse_args()
+    serial_communication = SerialCommunication(port, baudrate)
+    reading_thread, writing_thread, run_event = create_communication_threads(serial_communication)
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        run_event.clear()
+        reading_thread.join()
+        writing_thread.join()
+        exit(0)
+    finally:
+        exit(1)
 
 if __name__ == '__main__':
     main()
